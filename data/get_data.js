2c0ed949ad9b0939846a8014ea523ea7721f84b9
alvincf10 <alvinwijaya446@gmail.com>
2020-04-26 21:33:56 +0700

    revisi

--- data/get_data.js
+++ data/get_data.js
@@ -1,9 +1,12 @@
 function tampilkanSemuaMenu() {
     $.getJSON('data/pizza.json', function (data) {
         let menu = data.menu;
-        console.log(menu);
+        menu.sort(function (a, b) {
+            return sortAlphabet(a.nama, b.nama);
+            return a.harga - b.harga;
+        });
         $.each(menu, function (i, data) {
-            $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"> <img src="img/makanan/' + data.gambar + '" class="card-img-top"><div class="card-body"> <h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a> </div> </div></div >')
+            $('#daftar-menu').append('<div class="col-md-4"><div class="card mb-3"> <img src="img/makanan/' + data.gambar + '" class="card-img-top"><div class="card-body"> <h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">RP. ' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a> </div> </div></div >')
         });
     });
 }
@@ -19,7 +22,7 @@ $('.nav-link').on('click', function () {
     $('h1').html(kategori);
 
     if (kategori == 'All Menu') {
-        $('#daftar - menu').html('');
+        $('#daftar-menu').html('');
         tampilkanSemuaMenu();
         return;
     }
@@ -28,12 +31,22 @@ $('.nav-link').on('click', function () {
     $.getJSON('data/pizza.json', function (data) {
         let menu = data.menu;
         let content = '';
-
-        $.each(menu, function (i, data) {
-            if (data.kategori === kategori.toLowerCase()) {
-                content += '<div class="col-md-4"><div class="card mb-3"> <img src="img/makanan/' + data.gambar + '" class="card-img-top"><div class="card-body"> <h5 class="card-title">' + data.nama + '</h5><p class="card-text">' + data.deskripsi + '</p><h5 class="card-title">' + data.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a> </div> </div></div >'
+        menu.sort(function (a, b) {
+            return sortAlphabet(a.nama, b.nama);
+            return a.harga - b.harga;
+        });
+        $.each(menu, function (i, data2) {
+            if (data2.kategori == kategori.toLowerCase()) {
+                content += '<div class="col-md-4"><div class="card mb-3"> <img src="img/makanan/' + data2.gambar + '" class="card-img-top"><div class="card-body"> <h5 class="card-title">' + data2.nama + '</h5><p class="card-text">' + data2.deskripsi + '</p><h5 class="card-title">' + data2.harga + '</h5><a href="#" class="btn btn-primary">Pesan Sekarang</a> </div> </div></div >'
             }
         });
         $('#daftar-menu').html(content);
     });
 });
+
+function sortAlphabet(a, b) {
+    a = a.toLowerCase();
+    b = b.toLowerCase();
+
+    return (a < b) ? -1 : (a > b) ? 1 : 0;
+}
